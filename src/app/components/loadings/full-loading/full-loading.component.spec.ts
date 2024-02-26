df759a7ff58f1f1c00db70b8354f4fd7840f577c
Gabriel de Souza <gabriel.dsouzapro@gmail.com>
2024-02-26 16:37:35 -0300

    feat(component): create full screen loading component

---
+++ src/app/components/loadings/full-loading/full-loading.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FullLoadingComponent } from './full-loading.component';
+
+describe('FullLoadingComponent', () => {
+  let component: FullLoadingComponent;
+  let fixture: ComponentFixture<FullLoadingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FullLoadingComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FullLoadingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
