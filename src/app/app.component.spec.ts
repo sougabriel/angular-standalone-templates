eaa91bdcdb331a4be30817d2cbdb131428cbc504
Gabriel de Souza <gabriel.dsouzapro@gmail.com>
2024-04-08 21:46:32 -0300

    chore: update name of the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'standalone-templates' title`, () => {
+  it(`should have the 'angular-templates' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('standalone-templates');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, standalone-templates');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-templates');
   });
 });
