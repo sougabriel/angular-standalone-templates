08773b557406026db5e88b97f98a6c270906cbdc
Gabriel de Souza <gabriel.dsouzapro@gmail.com>
2024-04-16 15:13:02 -0300

    chore: update indent size and style

--- src/app/message-service/message.service.spec.ts
+++ src/app/message-service/message.service.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
-  let service: MessageService;
+	let service: MessageService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MessageService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		service = TestBed.inject(MessageService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 });
--- src/app/message-service/message.service.ts
+++ src/app/message-service/message.service.ts
@@ -1,30 +1,30 @@
 import { Injectable, inject } from '@angular/core';
 import {
-  MatSnackBar,
-  MatSnackBarConfig,
-  MatSnackBarRef,
+	MatSnackBar,
+	MatSnackBarConfig,
+	MatSnackBarRef,
 } from '@angular/material/snack-bar';
 import { ComponentType } from '@angular/cdk/portal';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class MessageService {
-  protected messageBar = inject(MatSnackBar);
-  protected matBarconfigs: MatSnackBarConfig = {};
+	protected messageBar = inject(MatSnackBar);
+	protected matBarconfigs: MatSnackBarConfig = {};
 
-  public openMessage(
-    text: string,
-    actionText: string = '',
-    configs: MatSnackBarConfig = this.matBarconfigs
-  ) {
-    return this.messageBar.open(text, actionText, configs);
-  }
+	public openMessage(
+		text: string,
+		actionText: string = '',
+		configs: MatSnackBarConfig = this.matBarconfigs
+	) {
+		return this.messageBar.open(text, actionText, configs);
+	}
 
-  public openMessageFromComponent(
-    component: ComponentType<any>,
-    configs: MatSnackBarConfig = this.matBarconfigs
-  ): MatSnackBarRef<any> {
-    return this.messageBar.openFromComponent(component, configs);
-  }
+	public openMessageFromComponent(
+		component: ComponentType<any>,
+		configs: MatSnackBarConfig = this.matBarconfigs
+	): MatSnackBarRef<any> {
+		return this.messageBar.openFromComponent(component, configs);
+	}
 }
