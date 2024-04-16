d07e039bbad6cf08d7f07b882b936fd9bbcaab8f
Gabriel de Souza <gabriel.dsouzapro@gmail.com>
2024-04-16 14:28:00 -0300

    chore: remove message component reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MessageComponent } from './message/message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MessageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
